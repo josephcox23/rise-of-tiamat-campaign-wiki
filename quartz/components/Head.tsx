f51cee4c052cdbdb936dabe8860b9b23872da43b
josephcox23 <josephcox23@gmail.com>
2026-06-24 12:46:14 -0400

    Update Head.tsx

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -30,9 +30,6 @@ export default (() => {
     const socialUrl =
       fileData.slug === "404" ? url.toString() : joinSegments(url.toString(), fileData.slug!)
 
-    const usesCustomOgImage = ctx.cfg.plugins.emitters.some(
-      (e) => e.name === CustomOgImagesEmitterName,
-    )
     const ogImageDefaultPath = `https://${cfg.baseUrl}/static/og-image.png`
 
     const coreStylesheet = css[0]?.content
